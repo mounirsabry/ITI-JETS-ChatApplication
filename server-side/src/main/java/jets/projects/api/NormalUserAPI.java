bcfa0a93ee4bd61f6c230d2d46d7cbd06525f2a7
mounirsabry <mounirsabry25@gmail.com>
2025-02-09 09:42:17 +0200

    Modified the apis to the correct contact invitation behaviour, added null from server to exception message

--- server-side/src/main/java/jets/projects/api/NormalUserAPI.java
+++ server-side/src/main/java/jets/projects/api/NormalUserAPI.java
@@ -132,7 +132,9 @@ public interface NormalUserAPI extends Remote {
     public boolean sendContactInvitation(ClientToken token,
             String userPhoneNumber) throws RemoteException;
     
-    public boolean acceptContactInvitation( ClientToken token,
+    // The return type contains the contact who sent 
+    // the contact invitation.
+    public ContactInfo acceptContactInvitation( ClientToken token,
             int invitationID) throws RemoteException;
     
     public boolean rejectContactInvitation(ClientToken token,
