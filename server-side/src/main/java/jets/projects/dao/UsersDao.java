e65e1e59ddf06ed04ef40a6f0070ac11948bba30
mounirsabry <mounirsabry25@gmail.com>
2025-02-11 17:08:39 +0200

    Finished UsersDao to NotificationDao

--- server-side/src/main/java/jets/projects/dao/UsersDao.java
+++ server-side/src/main/java/jets/projects/dao/UsersDao.java
@@ -1,13 +1,14 @@
 package jets.projects.dao;
 
-import java.sql.Blob;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.Date;
 import jets.projects.classes.RequestResult;
 import jets.projects.db_connections.ConnectionManager;
+import jets.projects.entities.Country;
 import jets.projects.session.ClientSessionData;
 import jets.projects.entities.Gender;
 import jets.projects.entities.NormalUser;
@@ -87,7 +88,7 @@ public class UsersDao {
                 + "(?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
         
         try (Connection connection = ConnectionManager.getConnection();
-            PreparedStatement stmt = connection.prepareStatement(query)) {
+            PreparedStatement stmt = connection.prepareStatement(query);) {
             int i = 1;
             stmt.setString(i++, user.getDisplayName());
             stmt.setString(i++, user.getPhoneNumber());
@@ -96,112 +97,160 @@ public class UsersDao {
             stmt.setString(i++, user.getPassword());
             stmt.setString(i++, user.getGender().toString());
             stmt.setString(i++, user.getCountry().toString());
-            stmt.setDate(i++, new java.sql.Date(user.getBirthDate().getTime()));
+            stmt.setDate(i++, new java.sql.Date(
+                    user.getBirthDate().getTime()));
             stmt.setString(i++, user.getBio());
             int rowsInserted = stmt.executeUpdate();
             return new RequestResult<>(rowsInserted > 0, null);
-        } catch (SQLException e) {
-            return new RequestResult<>(false, "Error registering user: " + e.getMessage());
+        } catch (SQLException ex) {
+            return new RequestResult<>(null, "DB Error: "
+                    + ex.getMessage());
         }
     }
     
     public RequestResult<NormalUser> getNormalUserProfile(int userID) {
         String query = "SELECT * FROM NormalUser WHERE user_ID = ?";
-        try (PreparedStatement preparedStatement = ConnectionManager.getConnection().prepareStatement(query)) {
-            preparedStatement.setInt(1, userID);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            if (resultSet.next()) {
-                NormalUser user = new NormalUser();
-                user.setUserID(resultSet.getInt("user_ID"));
-                user.setDisplayName((resultSet.getString("display_name")));
-                user.setEmail(resultSet.getString("email"));
-                user.setPhoneNumber(resultSet.getString("phone_number"));
-                user.setBirthDate(resultSet.getDate("birth_date"));
-                user.setGender(Gender.valueOf(resultSet.getString("gender")));
-                user.setPic(resultSet.getBlob("pic").getBytes(0,
-                        (int) resultSet.getBlob("pic").length()));
-                return new RequestResult<>(user, null);
-            } else {
-                return new RequestResult<>(null, "User not found");
+        
+        try (Connection connection = ConnectionManager.getConnection();
+            PreparedStatement stmt = connection.prepareStatement(query)) {
+            stmt.setInt(1, userID);
+            ResultSet resultSet = stmt.executeQuery();
+            
+            if (!resultSet.next()) {
+                return new RequestResult<>(null, null);
             }
-        } catch (SQLException e) {
-            return new RequestResult<>(null, "Error fetching user profile: " + e.getMessage());
+            NormalUser user = new NormalUser();
+            user.setUserID(
+                    resultSet.getInt("user_ID"));
+            user.setDisplayName(
+                    resultSet.getString("display_name"));
+            user.setPhoneNumber(
+                    resultSet.getString("phone_number"));
+            user.setEmail(
+                    resultSet.getString("email"));
+            user.setPic(
+                    resultSet.getBytes("pic"));
+            String genderString = resultSet.getString("gender");
+            user.setGender(
+                    Gender.valueOf(genderString));
+            String countryString = resultSet.getString("country");
+            user.setCountry(Country.valueOf(countryString));
+            user.setBirthDate(
+                    resultSet.getDate("birth_date"));
+            Timestamp createdAtStamp 
+                    = resultSet.getTimestamp("created_at");
+            user.setCreatedAt(createdAtStamp.toLocalDateTime());
+            String statusString = resultSet.getString("status");
+            user.setStatus(NormalUserStatus.valueOf(statusString));
+            user.setBio(
+                    resultSet.getString("bio"));
+            user.setIsAdminCreated(
+                    resultSet.getBoolean(
+                            "is_admin_created"));
+            user.setIsPasswordValid(
+                    resultSet.getBoolean(
+                            "is_password_valid"));
+            return new RequestResult<>(user, null);
+        } catch (SQLException ex) {
+            return new RequestResult<>(null, "DB Error: " 
+                    + ex.getMessage());
         }
     }
 
     public RequestResult<Boolean> editProfile(int userID, String username,
             Date birthDate, String bio, byte[] pic) {
-        String query = "UPDATE NormalUser SET display_name = ?, bio = ? ,pic = ? WHERE user_ID = ?";
-        try (PreparedStatement updateStatement = ConnectionManager.getConnection().prepareStatement(query)) {
-            updateStatement.setString(1, username);
-            updateStatement.setString(2, bio);
-            birthDate;
-            updateStatement.setBlob(3, pic);
-            updateStatement.setInt(4, userID);
-            int rowsAffected = updateStatement.executeUpdate();
+        String query = "UPDATE NormalUser"
+                + " SET"
+                + " display_name = ?,"
+                + " birth_date = ?,"
+                + " bio = ?,"
+                + " pic = ?"
+                + " WHERE user_ID = ?";
+        try (Connection connection = ConnectionManager.getConnection();
+            PreparedStatement stmt = connection.prepareStatement(query);) {
+            int i = 1;
+            stmt.setString(i++, username);
+            var sqlDate = new java.sql.Date(birthDate.getTime());
+            stmt.setDate(i++, sqlDate);
+            stmt.setString(i++, bio);
+            stmt.setBytes(i++, pic);
+            stmt.setInt(i++, userID);
+            
+            int rowsAffected = stmt.executeUpdate();
             return new RequestResult<>(rowsAffected > 0, null);
-        } catch (SQLException e) {
-            return new RequestResult<>(false, "Error updating profile: " + e.getMessage());
+        } catch (SQLException ex) {
+            return new RequestResult<>(null, "DB Error: " 
+                    + ex.getMessage());
         }
     }
 
     public RequestResult<byte[]> getNormalUserProfilePic(int userID) {
         String query = "SELECT pic FROM NormalUser WHERE user_ID = ?";
-        try (PreparedStatement preparedStatement = ConnectionManager.getConnection().prepareStatement(query)) {
-            preparedStatement.setInt(1, userID);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            if (resultSet.next()) {
-                Blob picBlob = resultSet.getBlob("pic");
-                byte[] pic = picBlob.getBytes(0, (int) picBlob.length());
-                return new RequestResult<>(pic, null);
-            } else {
+        
+        try (Connection connection = ConnectionManager.getConnection();
+            PreparedStatement stmt = connection.prepareStatement(query);) {
+            stmt.setInt(1, userID);
+            ResultSet resultSet = stmt.executeQuery();
+            
+            if (!resultSet.next()) {
                 return new RequestResult<>(null, null);
             }
-        } catch (SQLException e) {
-            return new RequestResult<>(null, null);
+            byte[] pic = resultSet.getBytes("pic");
+            return new RequestResult<>(pic, null);
+        } catch (SQLException ex) {
+            return new RequestResult<>(null, "DB Error: " 
+                    + ex.getMessage());
         }
     }
 
     public RequestResult<Boolean> isPasswordValid(int userID, String password) {
-        String query = "SELECT password FROM NormalUser WHERE user_ID = ?";
-        try (PreparedStatement preparedStatement = ConnectionManager.getConnection().prepareStatement(query)) {
-            preparedStatement.setInt(1, userID);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            if (resultSet.next() && resultSet.getString("password").equals(password)) {
-                return new RequestResult<>(true, null);
-            } else {
-                return new RequestResult<>(false, "Incorrect password.");
-            }
-        } catch (SQLException e) {
-            return new RequestResult<>(false, "Error checking password: " + e.getMessage());
+        String query = "SELECT password FROM NormalUser "
+                + " WHERE user_ID = ?"
+                + " AND password = ?";
+        try (Connection connection = ConnectionManager.getConnection();
+            PreparedStatement stmt = connection.prepareStatement(query);) {
+            stmt.setInt(1, userID);
+            stmt.setString(2, password);
+            ResultSet resultSet = stmt.executeQuery();
+            
+            return new RequestResult<>(resultSet.next(),
+                    null);
+        } catch (SQLException ex) {
+            return new RequestResult<>(null, "DB Error: " 
+                    + ex.getMessage());
         }
     }
 
-    public RequestResult<Boolean> updatePassword(int userID, String oldPassword, String newPassword) {
-        String query = "UPDATE NormalUser SET password = ? WHERE user_ID = ?";
-        try (PreparedStatement preparedStatement = ConnectionManager.getConnection().prepareStatement(query)) {
-            preparedStatement.setString(1, newPassword);
-            preparedStatement.setInt(2, userID);
-            int rowsAffected = preparedStatement.executeUpdate();
+    public RequestResult<Boolean> updatePassword(int userID,
+            String oldPassword, String newPassword) {
+        String query = "UPDATE NormalUser SET password = ?"
+                + " WHERE user_ID = ?";
+        try (Connection connection = ConnectionManager.getConnection();
+            PreparedStatement stmt = connection.prepareStatement(query);) {
+            stmt.setString(1, newPassword);
+            stmt.setInt(2, userID);
+            int rowsAffected = stmt.executeUpdate();
+            
             return new RequestResult<>(rowsAffected > 0, null);
-        } catch (SQLException e) {
-            return new RequestResult<>(false, "Error updating password: " + e.getMessage());
+        } catch (SQLException ex) {
+            return new RequestResult<>(null, "DB Error: " 
+                    + ex.getMessage());
         }
     }
     
     public RequestResult<Boolean> setOnlineStatus(int userID, NormalUserStatus newStatus) {
-        String query = "UPDATE NormalUser SET status = ? WHERE user_ID = ?";
-        try (PreparedStatement preparedStatement = ConnectionManager.getConnection().prepareStatement(query)) {
-            preparedStatement.setString(1, newStatus.toString());
-            preparedStatement.setInt(2, userID);
-            int rowsAffected = preparedStatement.executeUpdate();
-            if (rowsAffected == 1) {
-                return new RequestResult<>(true, null);
-            } else {
-                return new RequestResult<>(false, null);
-            }
-        } catch (SQLException e) {
-            return new RequestResult<>(false, "Error updating online status: " + e.getMessage());
+        String query = "UPDATE NormalUser SET status = ?"
+                + " WHERE user_ID = ?";
+        try (Connection connection = ConnectionManager.getConnection();
+            PreparedStatement stmt = connection.prepareStatement(query);) {
+            stmt.setString(1, newStatus.toString());
+            stmt.setInt(2, userID);
+            int rowsAffected = stmt.executeUpdate();
+            
+            return new RequestResult<>(rowsAffected > 0, null);
+        } catch (SQLException ex) {
+            return new RequestResult<>(null, "DB Error: "
+                    + ex.getMessage());
         }
     }
 }
